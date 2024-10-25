2d1ee084bd629f45d1d603f8c13d505bdc943155
samarth2534@gmail.com <samarth2534@gmail.com>
2024-10-25 18:41:31 +0530

    Done with Internationalization

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,13 @@ import { addUser, removeUser } from "../utils/userSlice";
 import { useDispatch } from "react-redux";
 import { LOGO, USER_AVATAR } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const user = useSelector((store) => store.user);
 
@@ -48,16 +51,32 @@ const Header = () => {
     dispatch(toggleGptSearchView());
   };
 
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={LOGO} alt="logo" />
       {user && ( //display it only if subscribed to store , don't display on main page
         <div className="flex p-2">
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white rounded-lg"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             className="py-2 px-4 m-2 bg-purple-800 text-white rounded-lg "
             onClick={handleGptSearchClick}
           >
-            Search
+            {showGptSearch ? "Home" : "Search"}
           </button>
           <img className="w-14 h-14" alt="userIcon" src={USER_AVATAR} />
 
